120312559aaa0e7b7c0d7b89893de3de6ee4a2e4
Peter Hudec <peterhudec@peterhudec.com>
2023-03-08 15:54:24 +0000

    Updated the call to action

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React or do something else
         </a>
       </header>
     </div>
